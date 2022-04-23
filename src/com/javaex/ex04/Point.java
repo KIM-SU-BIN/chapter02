cc144e27781082c299c439378c854e02dcb24f5c
subintoto <subintoto@naver.com>
2022-04-23 23:55:14 +0900

    point CL

--- src/com/javaex/ex04/Point.java
+++ src/com/javaex/ex04/Point.java
@@ -6,12 +6,24 @@ public class Point {
 	private int x;
 	private int y;
 
-	
 	// 생성자
+	public Point() {
+		//아무것도 없어보이지만 메모리에 올리는 일
+	}
+	public Point(int x) {
+		//아무것도 없어보이지만 메모리에 올리는 일
+	}
+	
+	public Point(int x, int y) {
+		//메모리에 올리는 일
+
+	}
 	
 	
 	// 매소드-gs
 	// 실제 진행되어야할 항목 기재 //void 기재할 항목이 없다는 뜻 원래int, double 이런식으로 들어가야함!
+	//set뒤에 오는 이름의 첫글자 항상 대문자
+	//int x, x=x;처럼 이름이 같으면 앞에 this.x=x;로 기입할 것
 	public void setX(int num) { 	
 		x = num;
 	}
@@ -29,5 +41,9 @@ public class Point {
 	// 매소드-일반
 	public void draw() {
 		System.out.println("점 [ x= " + x + "," + "y=" + y + "] 을 그렸습니다."); // x=__ y=__
+		
+		/*@Override
+		public String toString() {
+			return "Point [x=" + x + ", y=" + y + "]";*/
 	}
 }
--- src/com/javaex/ex04/PointApp.java
+++ src/com/javaex/ex04/PointApp.java
@@ -4,16 +4,25 @@ public class PointApp {
 
 	public static void main(String[] args) {
 
-		Point ex = new Point();
-		ex.setX(5);
-		ex.setY(5);
+		Point p1 = new Point();
+		p1.setX(5);
+		p1.setY(5);
 
-		Point ex2 = new Point();
-		ex2.setX(10);
-		ex2.setY(23);
-
-		ex.draw();
-		ex2.draw();
+		Point p2 = new Point();
+		p2.setX(10);
+		p2.setY(23);
+		
+		Point p3 = new Point(10, 20);
+		
+		Point p4 = new Point(100);
+		p4.setY(200);
+		
+		//y값을 먼저 넣고 setter x값을 넣는다.
+		
+		p1.draw();
+		p2.draw();
+		p3.draw();
+		
 	}
 
 }
