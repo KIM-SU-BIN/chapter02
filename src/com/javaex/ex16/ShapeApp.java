5ac3f9ff1ad117e0d84d5f4898afb08bf5246bf2
subintoto <subintoto@naver.com>
2022-04-30 18:26:07 +0900

    추상클래스, 인터페이스 0429

--- src/com/javaex/ex16/ShapeApp.java
+++ src/com/javaex/ex16/ShapeApp.java
@@ -1,6 +1,6 @@
 package com.javaex.ex16;
 
-public class ShapwApp {
+public class ShapeApp {
 	
 		public static void main(String[] args) {
 			//배열
--- src/com/javaex/ex17/ShapeApp.java
+++ src/com/javaex/ex17/ShapeApp.java
@@ -1,6 +1,6 @@
 package com.javaex.ex17;
 
-public class ShapwApp {
+public class ShapeApp {
 	
 		public static void main(String[] args) {
 			
